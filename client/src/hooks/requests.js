54801364e580d2fb1812c3d7e8c961484afb1c5b
woodylucas <wlucas0311@gmail.com>
2023-06-17 18:06:22 -0400

    feat: created post request on client side, and updated destination input to match clients target input name

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -15,8 +15,21 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    const response = await fetch(`${API_URL}/launches`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+
+    return response;
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
